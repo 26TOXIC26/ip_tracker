c6a3b2b073d07bc38482f007a709efdbab70c9bd
26TOXIC26 <bdmousaid@gmail.com>
2024-11-24 03:34:36 +0100

    2

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const api = "https://ipapi.co/json/";
+let data;
+
+async function getIP() {
+	const response = await fetch(api);
+	data = await response.json();
+	console.log(data);
+}
+
+getIP();
+
 var map = L.map('map').setView([51.505, -0.09], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 21,
