9905f21fc190d0b7e4ed2d9421475643408bad7a
26TOXIC26 <bdmousaid@gmail.com>
2024-12-02 07:33:40 +0100

    4

--- script.js
+++ script.js
@@ -20,50 +20,39 @@ function updateMap(latitude, longitude) {
 }
 
 function setData() {
+	console.log(data.ip);
+	  if (!data.ip)
+	  {
+		document.getElementById("ip-address-value").innerHTML = "No IP found";
+		document.getElementById("location-value").innerHTML = "No location found";
+		document.getElementById("timezone-value").innerHTML = "No timezone found";
+		document.getElementById("isp-value").innerHTML = "No ISP found";
+		document.getElementById("currency").innerHTML = "No currency found";
+		document.getElementById("languages").innerHTML = "No languages found";
+		document.getElementById("calling-code").innerHTML = "No calling code found";
+		document.getElementById("country").innerHTML = "No country found";
+		document.getElementById("capital").innerHTML = "No capital found";
+		return;
+	  }
 	  document.getElementById("ip-address-value").innerHTML = data.ip;
-	  console.log(data.ip);
 	  document.getElementById("location-value").innerHTML = data.city + ", " + data.country_name + " " + data.country_emoji;
-	  console.log(data.city + ", " + data.country_name);
 	  document.getElementById("timezone-value").innerHTML = data.time_zone.name;
-	  console.log(data.time_zone.name);
 	  document.getElementById("isp-value").innerHTML = data.isp;
-	  console.log(data.isp);
 	  document.getElementById("currency").innerHTML = data.currency.name + " (" + data.currency.code + ")";
-	  console.log(data.currency.name + " (" + data.currency.code + ")");
 	  document.getElementById("languages").innerHTML = data.languages.split(",")[0].toUpperCase();
-	  console.log(data.languages.split(",")[0].toUpperCase());
 	  document.getElementById("calling-code").innerHTML = data.calling_code;
-	  console.log(data.calling_code);
 	  document.getElementById("country").innerHTML = data.country_name_official;
-	  console.log(data.country_name_official);
 	  document.getElementById("capital").innerHTML = data.country_capital;
-	  console.log(data.country_capital);
 	updateMap(data.latitude, data.longitude);
 }
 
 async function getIP() {
 	const response = await fetch(apiURL, requestOptions);
 	data = await response.json();
-	console.log(apiURL);
-	console.log(data);
 	setData();
 }
 
-
 getIP();
-
-function show_more_info() {
-	if (document.getElementById("hidden-info").style.left == "75%") {
-		document.getElementById("more-info").style.transform = "translate(-50%, -50%) rotate(0deg)";
-		document.getElementById("hidden-info").style.left = "-100%";
-	}
-	else
-	{
-		document.getElementById("more-info").style.transform = "translate(-50%, -50%) rotate(180deg)";
-		document.getElementById("hidden-info").style.left = "75%";
-	}
-}
-
 
 function take_ip() {
 	ip = ipInput.value;
